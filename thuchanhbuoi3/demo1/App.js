be8c2f32e6b1f555f77733f7bd3d2e84c001c5ca
quanNguyen0872 <quan14139@gmail.com>
2022-09-15 10:35:22 +0700

    ver1

---
+++ thuchanhbuoi3/demo1/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Button } from "@react-native-material/core";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+      <Button title="Click Me" onPress={() => alert("🎉🎉🎉")}/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
