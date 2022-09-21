afbde44d0b138d4fb0ff29e9f4796eb6bbb57847
quanNguyen0872 <quan14139@gmail.com>
2022-09-21 23:50:36 +0700

    update screenshot demo1

--- thuchanhbuoi3/demo1/App.js
+++ thuchanhbuoi3/demo1/App.js
@@ -1,23 +1,81 @@
-import React from "react";
-import { Button } from "@react-native-material/core";
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Button } from '@react-native-material/core';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import rec from "./assets/rec.png";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-      <Button title="Click Me" onPress={() => alert("🎉🎉🎉")}/>
-    </View>
+    <LinearGradient
+      colors={['#c6ffdd', '#fbd786', '#f7797d']}
+      style={styles.linearGradient}
+    >
+      <View style={styles.container}>
+        <Image source={rec} style={styles.logo}/>
+        <Text style={styles.title}>Grown your business</Text>
+        <Text style={styles.text}>
+          We will help you to grow your business using online server
+        </Text>
+        <View style={styles.groupBtn}>
+          <Button title="Login" style={styles.btn} />
+          <Button title="Sign up" style={styles.btn} />
+        </View>
+        <Text style={styles.link}>HOW WE WORK?</Text>
+      </View>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "flex-start",
+    paddingTop: 50,
+    paddingLeft: 20,
+    paddingRight: 20,
   },
+  linearGradient: {
+    flex: 1,
+    paddingLeft: 15,
+    paddingRight: 15,
+    borderRadius: 5,
+  },
+  logo: {
+    width: 200,
+    height: 200,
+    marginBottom: 50,
+  },
+  title: {
+    marginBottom: 20,
+    fontSize: 32,
+    textTransform: "uppercase",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  text: {
+    textAlign: "center",
+    fontSize: 17,
+    fontWeight: "bold",
+    marginTop: 15,
+  },
+  groupBtn: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "100%",
+  },
+  btn: {
+    paddingLeft: 30,
+    paddingRight: 30,
+    paddingTop: 8,
+    paddingBottom: 8,
+    marginTop: 25,
+    backgroundColor: "#ed522f"
+  },
+  link: {
+    color: "#000",
+    fontSize: 20,
+    fontWeight: "bold",
+    marginTop: 50,
+  }
 });
